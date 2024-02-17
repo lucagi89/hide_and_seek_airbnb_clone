d0623759d994bcfceddd440638764a2efb95fe73
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2024-02-17 16:39:17 +0000

    created schema

--- db/migrate/20240217163028_create_flats.rb
+++ db/migrate/20240217163028_create_flats.rb
@@ -1,12 +1,12 @@
-class CreatePages < ActiveRecord::Migration[7.1]
+class CreateFlats < ActiveRecord::Migration[7.1]
   def change
-    create_table :pages do |t|
+    create_table :flats do |t|
       t.string :name
       t.string :location
       t.decimal :price_per_night
       t.text :description
       t.integer :number_of_guests
-      t.references :booking, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,51 @@
-ActiveRecord::Schema[7.1].define(version: 2024_02_17_143040) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2024_02_17_163621) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id", null: false
+    t.bigint "flat_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flat_id"], name: "index_bookings_on_flat_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "flats", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.decimal "price_per_night"
+    t.text "description"
+    t.integer "number_of_guests"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_flats_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.integer "rating"
+    t.text "comment"
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -15,4 +60,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_17_143040) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "flats"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "flats", "users"
+  add_foreign_key "reviews", "bookings"
 end
