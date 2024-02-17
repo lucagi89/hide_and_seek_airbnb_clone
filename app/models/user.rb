3e0df2bd4a6845a6479ffddc74d98abfd3a314ad
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2024-02-17 20:04:32 +0000

    user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :name, presence: true
+  has_many :flats
+  has_many :bookings
+  has_many :flats, through: :bookings
   has_one_attached :photo
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
