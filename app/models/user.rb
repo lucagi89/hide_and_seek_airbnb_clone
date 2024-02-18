a16f07ac21976256e03f3328670e293cef558a1c
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2024-02-18 11:03:21 +0000

    user validation change

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :email, presence: true
-  validates :password, presence: true
   validates :name, presence: true
   has_many :flats
   has_many :bookings
