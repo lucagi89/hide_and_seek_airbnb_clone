9018b59b1492048c207a4d9986048f21e4bf588b
Vincent Chaussepied <vincent.chaussepied@gmail.com>
2024-02-18 10:18:42 +0000

    Merge pull request #2 from lucagi89/add-validations
    Add validations

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,3 +1,15 @@
 class Flat < ApplicationRecord
-  belongs_to :user
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :price_per_night, presence: true
+  validates :description, presence: true
+  validates :number_of_guests, presence: true
+
+  belongs_to :user, through: :bookings
+  belongs_to :booking
+  has_many :bookings
+  has_many :users, through: :bookings
+  has_many :reviews, through: :bookings
+
+  has_many_attached :photos
 end
