aa14b779a2b56a02111b2744e7d62bc2a0ec7819
thomas fraser <thomas@thefrasers.co>
2024-02-20 19:22:26 +0000

    committed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "flats#home"
+  resources :flats, only: %i[new destroy] do
+    resources :bookings, only: %i[new create update]
+  end
+  resources :reviews, only: %i[show create destroy]
 end
---
+++ test/models/listed_property_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ListedPropertyTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
