878e4c40fbbc8b2ca99cb08aeabd910c56d33093
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2024-02-22 21:26:02 +0000

    implement controllers

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,12 +1,16 @@
 class FlatsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home show]
+  before_action :set_flat, only: %i[show]
 
   def home
     @flats = Flat.all
   end
 
+  def query
+    @flats = Flat.where("location ILIKE ?", "%#{params[:query]}%")
+  end
+
   def show
-    @flat = Flat.find(params[:id])
   end
 
   def create
@@ -22,6 +26,6 @@ class FlatsController < ApplicationController
   end
 
   def flat_params
-    params.require(flat).permit(:name, :photos, :location, :price_per_night, :description, :number_of_guests)
+    params.require(flat).permit(:name, :photo_url, :city, :address, :price_per_night, :description, :number_of_guests)
   end
 end
