a024f00d75437034d3a141e32f1c707f4562fd56
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2024-02-24 16:47:08 +0000

    updated

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -47,7 +47,7 @@ class BookingsController < ApplicationController
     @booking = Booking.find(params[:id])
   end
 
-  def bookink_params
+  def booking_params
     params.require(:booking).permit(:start_date, :end_date)
   end
 end
