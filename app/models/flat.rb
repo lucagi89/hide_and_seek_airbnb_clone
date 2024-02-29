0e18d07ac66a84d0a0bd478afdfa1409e8adcd66
thomas fraser <thomas@thefrasers.co>
2024-02-29 20:09:54 +0000

    updated

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -6,8 +6,6 @@ class Flat < ApplicationRecord
   validates :city, presence: true
   validates :address, presence: true
 
-  geocoded_by :address
-  after_validation :geocoded,if:
   geocoded_by :city
   after_validation :geocode, if:
   :will_save_change_to_address?
