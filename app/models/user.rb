724fe3e207ead8b2e75596082f972738457147e8
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2024-03-01 20:45:53 +0000

    added name on the sign up page, display name of user on the request cards

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :email, presence: true
-  # validates :name, presence: true
+  validates :name, presence: true
   has_many :flats
   has_many :bookings
   has_many :flats, through: :bookings
