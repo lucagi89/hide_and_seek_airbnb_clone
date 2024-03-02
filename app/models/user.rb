090498bfd0ef7ad70b2b9a56f248be640fdc0a82
Isaac Evans <isaacdnb94@gmail.com>
2024-03-02 10:38:00 +0000

    updatingbranch

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :email, presence: true
-  # validates :name, presence: true
+  validates :name, presence: true
   has_many :flats
   has_many :bookings
   has_many :flats, through: :bookings
-  # has_one_attached :photo
+  has_one_attached :photo
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
 end
